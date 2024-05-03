acbc4d264af567ef5e38f7d7ac643846c0253395
Nico Trummer <nictru32@gmail.com>
2024-05-03 14:27:39 +0000

    Add significance information to volcano plot

--- src/app.R
+++ src/app.R
@@ -186,13 +186,18 @@ server <- function(input, output, session) {
   output$cor_volcano <- renderPlotly({
     req(cor_result())
     data <- data.frame(rowData(cor_result()))
-    data$significant <- ifelse(data$qvalue < input$cor_alpha, "significant", "not significant")
+    data$significant <- ifelse(data$qvalue < input$cor_alpha,
+      "significant", "not significant"
+    )
     plot_ly(
       data,
       x = ~log2FC,
       y = ~ -log10(qvalue),
       color = ~significant,
-      text = ~ paste("Gene: ", rownames(data))
+      text = ~ paste(
+        "transcript: ", rownames(data),
+        "<br>qvalue: ", qvalue, "<br>log2FC: ", log2FC
+      )
     ) %>% add_markers()
   })
 }
